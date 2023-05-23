8117f13e57febd4c0dc29a6268a67bd3d9691e9b
PorfireanuRobert <robert.porfireanu@student.unitbv.ro>
2023-05-23 04:44:48 -0700

    Movement+Camera

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    public float force = 10; 
+    public float speed = 10; 
+    private float horizontal;
+    private float lockPos = 0;
+
+    Rigidbody2D rb;
+    private BoxCollider2D coli;
+
+    
+    private bool isJumping;
+
+    public LayerMask ground;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        coli = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("space"))
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
+
+        if (Input.GetKey("space") && IsGrounded())
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+            rb.velocity = Vector2.up * force;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coli.bounds.center, coli.bounds.size, 0f, Vector2.down, 0.1f, ground);
+
     }
 }
